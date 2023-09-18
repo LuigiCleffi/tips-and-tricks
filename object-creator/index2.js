2fb48a5c019bc2916c02db7ebb20fd1942c1e233
LuigiCleffi <luigicleff674@gmail.com>
2023-09-18 15:26:07 -0300

    factory function in Javascript

---
+++ object-creator/index2.js
@@ -0,0 +1,16 @@
+const pessoas = [
+  {
+    nome: "Luigi",
+    age: 22,
+    jobTitle: "Software Engineer"
+  },
+  {
+    nome: "Cloverson",
+    age: 39,
+    jobTitle: "Manobrista"
+  }
+]
+
+pessoas.forEach(client => {
+  console.log(client)
+})
