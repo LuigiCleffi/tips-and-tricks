07db7e48c14f7503398a11a14590ec61c984355e
Luigi Cleffi <luigicleffi674@gmail.com>
2024-05-28 23:07:25 -0300

    removed tips from fonts

--- closure/index.js
+++
@@ -1,9 +0,0 @@
-function outerFunction(x) {
-  return function innerFunction(y) {
-      return x + y;
-  };
-}
-
-const closure = outerFunction(10);
-const result = closure(5);
-console.log(result);
