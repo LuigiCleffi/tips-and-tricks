5c55ce2afbfd7a401f8838d950d85f5c68d4c2c9
Luigi Cleffi <luigicleff674@gmail.com>
2024-08-21 15:10:03 -0300

    feat: Update Button component to use spread props
    The code changes include:
    - Refactor the `InputField` component to `Button` component
    - Use spread props to pass all other props directly to the button element
    
    Recent user commits:
    - feat: Refactor TypeScript configuration for Vite project
    - feat: final version without routing.
    - feat: Update App component to display subject cards
    - feat: Add Card and NavBar components for displaying subject information
    
    Recent repository commits:
    - chore: Configure Tailwind CSS and PostCSS for the TypeScript project
    - chore: Update Vite configuration and TypeScript files
    - initial config for typescript
    - feat: Update React app with initial files for creating a component with Vite
    - feat: Add InputField component for spread component
    - chore: Update Node.js version requirement to 20.0.0 in package.json
    - feat: Add ChildrenComponent for rendering child components
    - feat: Add Counter component for declarative rendering
    - feat: update App.jsx
    - feat: Add UserAge component

--- creating_your_first_component/src/App.jsx
+++ creating_your_first_component/src/App.jsx
@@ -1,8 +1,15 @@
 import ChildrenComponent from "./components/ChildrenComponent"
 import Counter from "./components/DeclarativeRendering"
 import HelloWorld from "./components/HelloWorld"
-import InputField from "./components/SpreadComponent"
+import {Button} from "./components/SpreadComponent"
 import UserAge from "./components/UserAge"
+
+const buttonProps = {
+  label: "Click me",
+   onClick: () => alert("Button clicked!"),
+    disabled: false, 
+    color: "blue"
+}
 
 function App() {
   return (
@@ -14,7 +21,7 @@ function App() {
       <ChildrenComponent>
         <p>This is a child component!</p>
       </ChildrenComponent>
-      <InputField type="text" placeholder="Enter your name" className="input" />
+      <Button {...buttonProps} />
     </>
 )
 }
--- creating_your_first_component/src/components/SpreadComponent/index.jsx
+++ creating_your_first_component/src/components/SpreadComponent/index.jsx
@@ -1,5 +1,35 @@
-function InputField(props) {
-  return <input {...props} />;
-}
+import PropTypes from 'prop-types';
 
-export default InputField;
+// const Button = ({ label, onClick, disabled, color }) => {
+//   return   (
+//     <button onClick={onClick} disabled={disabled} style={{ backgroundColor: color }}>
+//       {label}
+//     </button>
+//   );
+// };
+
+
+/**
+ * 
+ *  Instead of manually passing each prop like onClick, disabled, and color,
+ *  we can use spread props to pass all other props directly to the button element.
+ *  
+ * */
+ 
+// eslint-disable-next-line react/prop-types
+function Button({ label, ...restProps }){
+  return (
+    <button {...restProps}>
+      {label}
+    </button>
+  );
+};
+
+export {Button};
+
+Button.prototype = {
+  label: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  disabled: PropTypes.bool.isRequired,
+  color: PropTypes.string.isRequired
+}
