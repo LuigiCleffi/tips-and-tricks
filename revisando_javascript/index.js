e285c13ae567a453ce8908ea8e434031e81a5a15
Luigi Cleffi <luigicleff674@gmail.com>
2024-08-22 10:26:33 -0300

    Enunciado da questão

---
+++ revisando_javascript/index.js
@@ -0,0 +1,8 @@
+/**
+ * Uma empresa recebe R$ 580.000,00 por ano e paga um desenvolvedor sênior
+ *  R$ 28.000,00 por mês. Além disso, a empresa paga 28% de impostos sobre o 
+ * valor do salário desse desenvolvedor. Considerando esses valores, qual é o 
+ * total gasto pela empresa com o salário e os impostos desse único 
+ * desenvolvedor, e quanto sobra do valor bruto mensal da empresa após
+ *  esse gasto?
+ */
